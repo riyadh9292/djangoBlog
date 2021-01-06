655cb96341ae1a09d4f49fa1c4b6f78580170e97
Riyadh <mrriyadh66@gmail.com>
2021-01-07 00:00:34 +0600

    first commit

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -135,4 +135,4 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='mrriyadh66@gmail.com'
-EMAIL_HOST_PASSWORD='Riyadh6666'
+EMAIL_HOST_PASSWORD='enter your email password'
